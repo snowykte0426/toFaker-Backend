cc54c1e5948713741f459f528d8ce3401e7a4722
snowykte0426 <snowykte0426@naver.com>
2024-08-28 20:29:14 +0900

    🔒feat: CORS 설정 추가 및 보안 설정 개선
    `SecurityConfig`에 CORS 설정을 추가하여 모든 출처와 메서드, 헤더를 허용하도록 구성했습니다. 또한, 모든 요청을 허용하면서 CSRF 보호를 비활성화하고, 폼 로그인 및 HTTP 기본 인증을 비활성화하여 보안 설정을 간소화했습니다.

--- src/main/kotlin/com/nextech/amond/global/security/SecurtiyConfig.kt
+++ src/main/kotlin/com/nextech/amond/global/security/SecurtiyConfig.kt
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
@@ -12,10 +15,25 @@ class SecurityConfig {
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
-        http.authorizeHttpRequests { authorizeRequests ->
+        http.cors { it.configurationSource(corsConfigurationSource()) }.csrf { it.disable() }
+            .authorizeHttpRequests { authorizeRequests ->
                 authorizeRequests.anyRequest().permitAll()
-            }.csrf { it.disable() }.cors { it.disable() }.formLogin { it.disable() }.httpBasic { it.disable() }
+            }.formLogin { it.disable() }.httpBasic { it.disable() }
 
         return http.build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration().apply {
+            allowedOrigins = listOf("*")
+            allowedMethods = listOf("*")
+            allowedHeaders = listOf("*")
+            allowCredentials = true
+        }
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 }
