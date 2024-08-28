6c28dd205e2ef1ac2c9b37f7e74c721e9e4515eb
snowykte0426 <snowykte0426@naver.com>
2024-08-28 17:17:53 +0900

    ➕feat: AWS S3 SDK 종속성 추가
    Amazon S3와의 통합을 위해 `aws-java-sdk-s3` 라이브러리(버전 1.12.691)를 프로젝트에 추가했습니다.

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("com.amazonaws:aws-java-sdk-s3:1.12.691")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
