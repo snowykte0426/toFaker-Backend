775ce87a2f459d89484e64e24fbe82d88c6dde59
snowykte0426 <snowykte0426@naver.com>
2024-08-28 20:07:43 +0900

    ✨feat: CORS 설정 및 기본 보안 설정 추가
    프로젝트에 CORS 설정과 기본 보안 구성을 추가했습니다. 모든 도메인에서의 요청을 허용하도록 설정하고, CSRF 보호를 비활성화했습니다.

---
+++ src/main/kotlin/com/nextech/amond/global/security/SecurtiyConfig.kt
@@ -0,0 +1,32 @@
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http.cors { it.configurationSource(corsConfigurationSource()) }.csrf { it.disable() }
+        return http.build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration().apply {
+            allowedOrigins = listOf("*")
+            allowedMethods = listOf("*")
+            allowedHeaders = listOf("*")
+            allowCredentials = true
+        }
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+}
